85c8843df3d7e745e8bf022cffa76c9c9d28d1fb
Alex Aguirre <aguirre_alex@rocketmail.com>
2021-09-23 04:34:00 -0500

    completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,27 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {isBioEmpty(props)}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links 
+        github={props.links.github} 
+        linkedin={props.links.linkedin}
+      />
     </div>
   );
+}
+
+function isBioEmpty(props){
+  if(!props.bio){
+    return null
+  }else{
+    return <p>{props.bio}</p>
+  }
 }
 
 export default About;
